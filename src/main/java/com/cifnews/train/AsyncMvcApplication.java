e44d2fd7e2620d8f3e49b5896299643c1254d333
zhao.dingyi@foxmail.com <zhao.dingyi@foxmail.com>
2018-04-17 17:20:17 +0800

    init project

---
+++ src/main/java/com/cifnews/train/AsyncMvcApplication.java
@@ -0,0 +1,12 @@
+package com.cifnews.train;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsyncMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsyncMvcApplication.class, args);
+	}
+}
---
+++ src/test/java/com/cifnews/train/AsyncMvcApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cifnews.train;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AsyncMvcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
